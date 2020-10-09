978037041c70da0c3cde98369ba3c29ec8cc9a6c
lucakbetter <lucakbetter@hotmail.com>
2020-10-09 16:39:37 +0200

    Update regression.py

--- regression.py
+++ regression.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.preprocessing import scale
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold
+from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.metrics import r2_score
 from load_data import dataframes, impute
 from outlier_detection import detect_outliers
@@ -15,6 +16,8 @@ x_train_outliers = detect_outliers(x_train)
 x_train = x_train[x_train_outliers==1]
 y_train = y_train[x_train_outliers==1]
 x_train = scale(x_train)
+x_train = np.concatenate((x_train, x_train*2), axis=1)
+x_train = SelectKBest(f_classif, k=90).fit_transform(x_train, y_train)
 
 sum_ = 0
 for train_idx, test_idx in kf.split(x_train):
@@ -23,4 +26,4 @@ for train_idx, test_idx in kf.split(x_train):
     y_pred = reg.predict(x_train[test_idx])
     sum_ += r2_score(y_train[test_idx], y_pred)
     print(r2_score(y_train[test_idx], y_pred))
-print('average: %s' % str(sum_/10))
+print('average score: %s' % str(sum_/10))
