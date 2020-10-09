1a9dd5edfdf8051da9cdd4ba510772c6ff3fec98
Dion <dionhs@hotmail.co.uk>
2020-10-09 17:12:16 +0200

    add different regression models

--- regression.py
+++ regression.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.preprocessing import scale
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
 from sklearn.model_selection import KFold
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.metrics import r2_score
@@ -10,19 +12,21 @@ n_splits=3
 
 x_train, y_train, x_test = dataframes()
 x_train = x_train.values
-y_train = y_train.values
+y_train = y_train.values.reshape(-1)
 x_train = impute(x_train)
 kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
 x_train_outliers = detect_outliers(x_train)
 x_train = x_train[x_train_outliers==1]
 y_train = y_train[x_train_outliers==1]
 x_train = scale(x_train)
-x_train = np.concatenate((x_train, x_train*2), axis=1)
-x_train = SelectKBest(f_classif, k=90).fit_transform(x_train, y_train)
+x_train = SelectKBest(f_classif, k=50).fit_transform(x_train, y_train)
 
 total_score = 0
 for train_idx, test_idx in kf.split(x_train):
-    reg = LinearRegression()
+    #reg = LinearRegression()
+    #reg = Ridge(alpha=100)
+    #reg = DecisionTreeRegressor()
+    reg = SVR(kernel='rbf', C=10.0)
     reg.fit(x_train[train_idx], y_train[train_idx])
     y_pred = reg.predict(x_train[test_idx])
     total_score += r2_score(y_train[test_idx], y_pred)
