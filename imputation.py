a76cb3338ce7ec4124dbea4022c95a9cad5976b1
Dion <dionhs@hotmail.co.uk>
2020-10-14 13:25:04 +0200

    fix imputation syntax

--- imputation.py
+++ imputation.py
@@ -5,14 +5,14 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import SimpleImputer, KNNImputer, IterativeImputer
 
 def impute(df, strategy):
-	if strategy == 'simple'
+    if strategy == 'simple':
     	imputer = SimpleImputer(strategy='mean')
-	elif strategy == 'knn':
-		imputer = KNNImputer()
-	elif strategy == 'iterative':
+    elif strategy == 'knn':
+        imputer = KNNImputer()
+    elif strategy == 'iterative':
     	imputer = IterativeImputer(n_nearest_features=100, verbose=5, tol=1e-5)
-	else:
-		print('no valid strategy selected. Available imputation strategies: simple, knn or iterative')
-	imputed = imputer.fit_transform(df)
-	imputed_df = pd.DataFrame(data=imputed)
-	return imputed_df
+    else:
+        print('no valid strategy selected. Available imputation strategies: simple, knn or iterative')
+    imputed = imputer.fit_transform(df)
+    imputed_df = pd.DataFrame(data=imputed)
+    return imputed_df
