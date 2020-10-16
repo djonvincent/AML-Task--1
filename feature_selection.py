5293398767c5f002bde07ff22570652085759a08
lucakbetter <lucakbetter@hotmail.com>
2020-10-16 22:12:48 +0200

    feature_selection new function

---
+++ feature_selection.py
@@ -0,0 +1,8 @@
+from sklearn.feature_selection import SelectKBest
+
+def kBest(x, y, x_test, function, k_features):
+	k_best = SelectKBest(function, k=k_features).fit(x, y)
+	x = k_best.transform(x)
+	x_test = k_best.transform(x_test)
+	return x, x_test
+	
